e7be2dc40f49bd6ac69960099582e2dca0f2c82f
kalp19219-rgb <kalp19219@gmail.com>
2025-08-28 16:15:19 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,7 @@
-def call() {
-    sh 'mvn test'
+stage('Unit Test Maven') {
+    steps {
+        dir('kalp_java') {   // jo pom.xml kalp_java folder ni andar hoy to
+            sh 'mvn test'
+        }
+    }
 }
